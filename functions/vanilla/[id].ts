ee86f3eb87875b05aa270ea97888797cf7b89feb
Juho Vepsäläinen <bebraw@gmail.com>
2024-05-16 10:13:42 +0300

    chore: Add a stub for vanilla benchmark

---
+++ functions/vanilla/[id].ts
@@ -0,0 +1,49 @@
+import { postTemplateWithComments } from "../../templates/vanilla.ts";
+import type { Post } from "../../types.ts";
+
+export async function onRequest({
+  env,
+  params: { id },
+  request: { url },
+}: {
+  env: { COMMENTS: KVNamespace };
+  params: { id: string };
+  request: Request;
+}) {
+  const res = await fetch(`${new URL(url).origin}/api/posts`);
+  const posts: Post[] = await res.json();
+  const foundPost = posts.find((p) => p.id === id);
+
+  if (!foundPost) {
+    return new Response(`{ "error": "No matching post was found" }`, {
+      status: 500,
+      headers: { "content-type": "application/json" },
+    });
+  }
+
+  /*
+  let comments = [];
+
+  try {
+    const data = await env.COMMENTS.get(id);
+
+    if (data) {
+      comments = JSON.parse(data);
+    }
+  } catch (error) {}
+  */
+
+  return new Response(
+    postTemplateWithComments({
+      ...foundPost,
+      comments: [],
+      base: "/vanilla/",
+    }),
+    {
+      status: 200,
+      headers: {
+        "content-type": "text/html;charset=UTF-8",
+      },
+    },
+  );
+}
