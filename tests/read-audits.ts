3f8ea331174a1c818c39dca44265ed1655bfa328
Juho Vepsäläinen <bebraw@gmail.com>
2024-06-25 10:57:19 +0300

    fix: Aggregate FCP for stepwise tests

--- tests/read-audits.ts
+++ tests/read-audits.ts
@@ -8,7 +8,23 @@ function readAudits(pageType: string, auditType: string) {
     .map((f) => fs.readFileSync(f, { encoding: "utf-8" }))
     .map((d) => JSON.parse(d));
 
-  return audits.map((a) => a["audits"][auditType]["numericValue"]);
+  return audits.map((a) => {
+    if (a.audits) {
+      return a.audits[auditType].numericValue;
+    }
+
+    if (a.steps) {
+      return a.steps
+        .map(
+          // @ts-expect-error This is fine for now. Maybe
+          // the exact type is available through Lighthouse
+          (s) => s.lhr.audits[auditType]?.numericValue,
+        )
+        .reduce((a: number, b: number) => a + b, 0);
+    }
+
+    throw new Error("Missing audits or steps property");
+  });
 }
 
 export { readAudits };
