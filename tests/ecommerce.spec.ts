cb991a95cd04b0075c333d2df63d8d71266869eb
Juho Vepsäläinen <bebraw@gmail.com>
2024-10-03 10:05:16 +0300

    refactor: Simplify ecommerce test setup

--- tests/ecommerce.spec.ts
+++ tests/ecommerce.spec.ts
@@ -2,69 +2,60 @@
 // with playwright yet!
 import fs from "fs";
 import { mkdirp } from "mkdirp";
-import { Cluster } from "puppeteer-cluster";
-import { type Page } from "puppeteer";
+import puppeteer, { type Page } from "puppeteer";
 import { startFlow } from "lighthouse";
-// import { printCSV } from "./print-csv.ts";
 import { range } from "./math.ts";
 
-// TODO: Consider crashing if any selector times out or if test throws
 async function main() {
   const amountOfRuns = 5;
   const testPrefix = "cf";
   const testTypes = ["ecommerce-ssr", "ecommerce-islands"];
 
-  // TODO: Consider using Puppeteer directly instead of clustering
-  // Prepare an execution cluster
-  const cluster = await Cluster.launch({
-    concurrency: Cluster.CONCURRENCY_CONTEXT,
-    // It is important to limit concurrency to one as otherwise
-    // results don't seem to make sense for some reason (cache?).
-    maxConcurrency: 1,
-  });
-
-  // Add a task
-  cluster.task(ecommerceTest);
-
-  // Execute test permutations
-  getTests(
+  runTests(
     amountOfRuns,
     testPrefix,
     "https://comments-benchmark.pages.dev",
     testTypes,
-  ).forEach((t) => cluster.queue(t));
-
-  // Shut down
-  await cluster.idle();
-  await cluster.close();
-
-  // Finish
-  // printCSV(amountOfRuns, testPrefix, testTypes);
+  );
 }
 
 main();
 
-// Generate test permutations
-function getTests(
+async function runTests(
   amountOfRuns: number,
   type: string,
   prefix: string,
   names: string[],
 ) {
-  return range(amountOfRuns).flatMap((i) =>
+  // Generate test permutations to run
+  const testConfigurations = range(amountOfRuns).flatMap((i) =>
     names.map((name: string) => ({ type, prefix, name, n: i + 1 })),
   );
+
+  const browser = await puppeteer.launch();
+
+  for (const configuration of testConfigurations) {
+    const page = await browser.newPage();
+
+    try {
+      await ecommerceTest({ page, configuration });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  await browser.close();
 }
 
 // Adapted from https://github.com/GoogleChrome/lighthouse/blob/main/docs/user-flows.md
 async function ecommerceTest({
   page,
-  data,
+  configuration,
 }: {
   page: Page;
-  data: { type: string; prefix: string; name: string; n: number };
+  configuration: { type: string; prefix: string; name: string; n: number };
 }) {
-  const { type, prefix, name, n } = data;
+  const { type, prefix, name, n } = configuration;
   const url = `${prefix}/${name}`;
 
   console.log("Testing", url, "run", n);
@@ -104,23 +95,19 @@ async function ecommerceTest({
 
   console.log("Writing report for run", n);
 
-  try {
-    // Phase 3 - Write a flow report.
-    await mkdirp("report-output");
-    await fs.promises.writeFile(
-      `report-output/${name}-${type}-${n}-report.html`,
-      await flow.generateReport(),
-    );
+  // Phase 3 - Write a flow report.
+  await mkdirp("report-output");
+  await fs.promises.writeFile(
+    `report-output/${name}-${type}-${n}-report.html`,
+    await flow.generateReport(),
+  );
 
-    // Phase 4 - Save results as JSON.
-    await mkdirp("benchmark-output");
-    await fs.promises.writeFile(
-      `benchmark-output/${name}-${type}-${n}-audit.json`,
-      JSON.stringify(await flow.createFlowResult(), null, 2),
-    );
+  // Phase 4 - Save results as JSON.
+  await mkdirp("benchmark-output");
+  await fs.promises.writeFile(
+    `benchmark-output/${name}-${type}-${n}-audit.json`,
+    JSON.stringify(await flow.createFlowResult(), null, 2),
+  );
 
-    console.log("Wrote reports for run", n);
-  } catch (error) {
-    console.error(error);
-  }
+  console.log("Wrote reports for run", n);
 }
